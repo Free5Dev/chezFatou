8861cc7062923ed9a430984c5e4044a6b263c9da
Free5Dev <saidsoumah@gmail.com>
2019-08-01 01:00:54 +0200

    'encour'

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- css -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKD_6nrk70W-ZzicH30-Y2dIYWreKevlU94iBXL2s8mM4ef1V3" type="image/x-icon">
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Pacifico&display=swap" rel="stylesheet">
+    <!-- font-family: 'Pacifico', cursive;  pour chez fatou-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <!-- font-family: 'Pacifico', cursive; -->
+    <title>Chez Fatou</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
+        <a class="navbar-brand text-white" href="#">Chez <span class="">Fatou</span></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
+            <ul class="navbar-nav ">
+            <li class="nav-item active">
+                <a class="nav-link text-white" href="#">Plâts </a>
+            </li>
+            <li class="nav-item text-white" >
+                <a class="nav-link text-white" href="#">But</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Contact</a>
+            </li>
+            <!-- <li class="nav-item">
+                <a class="nav-link text-white" href="#">Contact</a>
+            </li> -->
+            </ul>
+            
+        </div>
+    </nav>
+   <!-- banniere image -->
+   <div class="container-fluid" id="banniere">
+       <h1 class="display-3">Bienvenue chez Fatou Restaurant Africain</h1>
+    </div>
+    <!-- plats -->
+    <div class="container-fluid ">
+        <div class="row">
+            <div class="col-md-12 img">
+                <img src="http://placehold.it/200x200" alt="">
+                <img src="http://placehold.it/200x200" alt="">
+                <img src="http://placehold.it/200x200" alt="">
+                <img src="http://placehold.it/200x200" alt="">
+            </div>
+            <div class="col-md-12 img">
+            <img src="http://placehold.it/200x200" alt="">
+                <img src="http://placehold.it/200x200" alt="">
+                <img src="http://placehold.it/200x200" alt="">
+                <img src="http://placehold.it/200x200" alt="">
+            </div>
+        </div>
+    </div>
+    
+<!-- bootstrap js -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- script js -->
+</body>
+</html>
